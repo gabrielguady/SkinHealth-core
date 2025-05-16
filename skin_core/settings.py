d96520673eaa153f665dcb6f743fd899839e0212
gabriel.guady <gabriel.guady@fpf.br>
2025-05-16 10:27:18 -0300

    initial commit

--- skin_core/settings.py
+++ skin_core/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'skin_core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'skin',
-        'USER': 'postgres',
-        'PASSWORD': 'fpf2025',
-        'HOST': 'db-skin',
-        'PORT': '5432',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DB_NAME', 'skin'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'db-skin'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     },
 }
 
