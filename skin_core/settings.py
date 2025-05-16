74bfbbec6fd66f59bde6a3dbf866ce13bdc40871
junior <juniorsilvaoficial709@gmail.com>
2025-05-15 21:04:36 -0400

    test

--- skin_core/settings.py
+++ skin_core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'skin_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': 'db-skin',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
         'HOST': os.getenv('DB_HOST'),
